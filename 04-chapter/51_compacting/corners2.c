675d530aa5c8b52450de4d2213f3faa1bb6687d8
Imil Ametov <charlz_klug@mail.ru>
2024-03-01 17:33:53 +0300

    Chapter 4. Compacting

---
+++ 04-chapter/51_compacting/corners2.c
@@ -0,0 +1,30 @@
+#include <ncurses.h>
+
+int main(void) {
+  int rows, cols;
+
+  initscr();
+  getmaxyx(stdscr, rows, cols);
+  rows--;
+  cols--;
+
+  mvaddch(0, 0, '*');
+  refresh();
+  napms(500);
+
+  mvaddch(0, cols, '*');
+  refresh();
+  napms(500);
+
+  mvaddch(rows, 0, '*');
+  refresh();
+  napms(500);
+
+  mvaddch(rows, cols, '*');
+  refresh();
+
+  getch();
+
+  endwin();
+  return 0;
+}
