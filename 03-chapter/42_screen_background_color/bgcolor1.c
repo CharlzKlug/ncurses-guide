21dc270be98898f7d1dfd667d308a1c28a5bc2ce
Imil Ametov <charlz_klug@mail.ru>
2024-03-01 12:16:57 +0300

    Chapter 3. Some modifications

--- 03-chapter/42_screen_background_color/bgcolor1.c
+++ 03-chapter/42_screen_background_color/bgcolor1.c
@@ -5,7 +5,7 @@ int main(void) {
 
   start_color();
   init_pair(1, COLOR_WHITE, COLOR_BLUE);
-  bkgd(COLOR_PAIR(1));
+  bkgd(COLOR_PAIR(1) | '.');
   addstr("So this is what a color screen looks like?\n");
   refresh();
   getch();
