8c9cf8b62de18c344cac50aec70d523334ca245d
Imil Ametov <charlz_klug@mail.ru>
2024-03-01 17:33:28 +0300

    Chapter 4. Cornered

---
+++ 04-chapter/50_cornered/corners1.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+
+int main(void) {
+  int rows, cols;
+
+  initscr();
+  getmaxyx(stdscr, rows, cols);
+  rows--;
+  cols--;
+
+  move(0, 0);
+  addch('*');
+  refresh();
+  napms(500);
+
+  move(0, cols);
+  addch('*');
+  refresh();
+  napms(500);
+
+  move(rows, 0);
+  addch('*');
+  refresh();
+  napms(500);
+
+  move(rows, cols);
+  addch('*');
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
