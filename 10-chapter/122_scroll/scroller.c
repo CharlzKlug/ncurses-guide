71d22098b9c02579b67f2b05979c810a8ae778c0
Imil Ametov <charlz_klug@mail.ru>
2024-03-25 10:27:20 +0300

    Chapter 10. Can it scroll?

---
+++ 10-chapter/122_scroll/scroller.c
@@ -0,0 +1,40 @@
+#include <ncurses.h>
+
+#define FILENAME "gettysburg.txt"
+
+int main(void) {
+  FILE *text;
+  WINDOW *lister;
+  int maxy, maxx, ch;
+
+  initscr();
+  refresh();
+  getmaxyx(stdscr, maxy, maxx);
+
+  lister = newwin(maxy, maxx/2, 0, maxx/4);
+  if (lister == NULL) {
+    addstr("unable to create window\n");
+    refresh(); getch();
+    endwin();
+    return(1);
+  }
+
+  text= fopen(FILENAME,  "r");
+  if (text == NULL) {
+    addstr("unable to open file\n");
+    refresh(); getch();
+    endwin();
+    return(2);
+  }
+
+  do {
+    ch = fgetc(text);
+    waddch(lister, ch);
+    wrefresh(lister);
+  } while (ch != EOF);
+  fclose(text);
+  getch();
+
+  endwin();
+  return (0);
+}
