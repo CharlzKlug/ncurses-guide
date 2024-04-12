9131848783c3ae760ff1cb5039bd35726d9ed2bd
Imil Ametov <charlz_klug@mail.ru>
2024-04-12 16:27:07 +0300

    Chapter 12. Making the Index Line

---
+++ 12-chapter/151_index_line/softies2.c
@@ -0,0 +1,26 @@
+#include <ncurses.h>
+
+#define LMAX 12
+#define CENTER 1
+
+int main(void) {
+  char label_text[LMAX][6]= {
+    "Help", "File", "Edit", "Frmt",
+    "View", "Switch", "Win", "Help"
+  };
+  int label;
+
+  slk_init(3);
+  initscr();
+  slk_restore();
+  
+  for (label= 0; label < LMAX; label++)
+    slk_set(label+1, label_text[label], CENTER);
+
+  slk_refresh();
+
+  getch();
+
+  endwin();
+  return 0;
+}
