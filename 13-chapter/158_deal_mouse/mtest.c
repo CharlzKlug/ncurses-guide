5da8941a4ff3a0a19e16dc2b79a8fbcb22d96ede
Imil Ametov <charlz_klug@mail.ru>
2024-04-18 09:55:02 +0300

    Chapter 13. Can NCurses Deal with the Mouse

---
+++ 13-chapter/158_deal_mouse/mtest.c
@@ -0,0 +1,17 @@
+#include <ncurses.h>
+
+int main(void) {
+  /* mmask_t mmask; */
+
+  initscr();
+
+  if (NCURSES_MOUSE_VERSION > 0)
+    addstr("This version of NCurses supports the mouse.\n");
+  else
+    addstr("This version of NCurses does not support the mouse.\n");
+
+  refresh();
+  getch();
+  endwin();
+  return 0;
+}
